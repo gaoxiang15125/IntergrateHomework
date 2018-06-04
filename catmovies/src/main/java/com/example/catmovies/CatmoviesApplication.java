84dd1aeac5218189ba20ef0c7e561ffe234724a1
151250010 <851642452@qq.com>
2018-06-04 13:41:35 +0800

    add some unversion files

---
+++ catmovies/src/main/java/com/example/catmovies/CatmoviesApplication.java
@@ -0,0 +1,12 @@
+package com.example.catmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatmoviesApplication.class, args);
+    }
+}
---
+++ taobaomovies/src/main/java/com/example/taobaomovies/TaobaomoviesApplication.java
@@ -0,0 +1,12 @@
+package com.example.taobaomovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaobaomoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaobaomoviesApplication.class, args);
+    }
+}
